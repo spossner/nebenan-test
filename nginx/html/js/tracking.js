35570c9480ad997b979920a1b6e995227674b6c0
spossner <github@possner.de>
2021-09-05 09:13:57 +0200

    some more tracking and HTML tests

---
+++ nginx/html/js/tracking.js
@@ -0,0 +1,4 @@
+    var _mtm = window._mtm = window._mtm || [];
+    _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='http://localhost/js/container_BLgsM6Id.js'; s.parentNode.insertBefore(g,s);
